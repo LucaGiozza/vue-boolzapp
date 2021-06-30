0508a368eb5b2396733f3be37180403d9758d1f0
Luca Giozza <luca.giozza@gmail.com>
2021-06-30 16:26:22 +0200

    prova invio messaggio

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@
 
           },
           contactActive: 0,
+          scritta : '',
 
           contacts: [
               {
@@ -157,7 +158,15 @@
              setContactActive(index){
                  this.contactActive = index;
 
-             }
+             },
+
+             
+
+              inviaMessaggioUtente(){
+               
+
+             },
+
                 
              }
 
@@ -165,55 +174,28 @@
      );
 
 
-
+// Milestone 2
 //   Visualizzazione dinamica dei messaggi:
 //  tramite la direttiva v-for,
 //  visualizzare tutti i
 //  messaggi relativi al 
 // contatto attivo all’interno del pannello della conversazione
 //  Click sul contatto mostra la
-//  conversazione del contatto cliccato
+//  conversazione del contatto cliccato.
 
 
-// function stampaMessaggi(array){
-
-    
-  
-//     // document.getElementById('centro').innerHTML = '';
-  
-  
-    
-//    array.forEach((element) =>{
-   
-  
-  
-  
- 
-//    const {text, date} = element;
-//    const elementHtml =  `
-//    <section id="centro" class="centro">
-
-//    <div class="messaggi-inviati">
-//        <p class="${text}${date}"></p>
-
-//    </div>
-//    <div class="messaggi-ricevuti">
-//        <p class="${text}${date}"></p>
-
-//    </div>
-   
+ // Milestone 3
+//   Aggiunta di un messaggio: l’utente scrive 
+// un testo nella parte bassa e digitando
+ // “enter” il testo viene aggiunto al thread sopra,
+//  come messaggio verde
+ //  Risposta dall’interlocutore: ad ogni inserimento
+//  di un messaggio, l’utente riceverà
+ // un “ok” come risposta, che apparirà
+//  dopo 1 secondo.
 
 
 
-// </section>
-
-//    `;
-  
-//     document.getElementById('centro').innerHTML += elementHtml ;
-  
-//    });
-  
-//   }
 
 
 
